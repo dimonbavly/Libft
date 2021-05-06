84555ad2e290fcbff0f5bdfc37c1bddd4360bde4
Stannis Rupert <srupert@pr-a3.kzn.21-school.ru>
2021-05-06 17:43:46 +0300

    fix ft_memchr to norme

--- ft_memchr.c
+++ ft_memchr.c
@@ -1,15 +1,16 @@
 #include "libft.h"
-void *ft_memchr(const void *s, int c, size_t n)
+
+void	*ft_memchr(const void *s, int c, size_t n)
 {
-	unsigned char ch;
-	unsigned char *str;
-	
+	unsigned char	ch;
+	unsigned char	*str;
+
 	ch = (unsigned char) c;
-	str = (unsigned char*) s;
+	str = (unsigned char *) s;
 	while (n)
 	{
 		if (*str == ch)
-				return (str);
+			return (str);
 		str++;
 		n--;
 	}
