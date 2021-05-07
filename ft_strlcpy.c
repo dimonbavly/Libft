97115b01f968bd676a460ad9ea4cd8e42b457d29
Stannis Rupert <srupert@un-g4.kzn.21-school.ru>
2021-05-07 10:16:16 +0300

    ft_strlcpy test passed

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -1,10 +1,16 @@
 #include "libft.h"
+
 size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
-	while(size-1)
+	size_t	len;
+
+	len = strlen(src);
+	if (len + 1 < size)
+		memcpy(dst, src, len + 1);
+	else if (size != 0)
 	{
-		*dst++ = *src++;
-		size--;
+		memcpy(dst, src, size - 1);
+		dst[size - 1] = '\0';
 	}
-	return (ft_strlen(src));
+	return (len);
 }
