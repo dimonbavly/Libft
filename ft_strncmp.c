efa952e18a60067b6111da4ca5c19aa42e6e3850
Stannis Rupert <srupert@un-g4.kzn.21-school.ru>
2021-05-10 18:46:11 +0300

    fix ft_strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -7,7 +7,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	tmp = 0;
 	if (n == 0)
 		return (0);
-	while (s1[tmp] == s2[tmp] && tmp + 1 < n)
+	while (s1[tmp] == s2[tmp] && tmp + 1 < n && s1[tmp] && s2[tmp])
 		tmp++;
 	return ((unsigned char)s1[tmp] - (unsigned char)s2[tmp]);
 }
