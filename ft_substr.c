7c860f2d447d37ac04e3c2e16edf262ca903a43a
Stannis Rupert <srupert@un-g4.kzn.21-school.ru>
2021-05-11 11:56:08 +0300

    ft_substr fix

---
+++ ft_substr.c
@@ -0,0 +1,23 @@
+#include "libft.h"
+#include <stdlib.h>
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*res;
+
+	if ((size_t)start >= len)
+	{
+		res = malloc(sizeof(*res));
+		if (!res)
+			return (NULL);
+		*res = '\0';
+		return (res);
+	}
+	res = malloc(sizeof(*res) * len + 1);
+	if (!res)
+		return (NULL);
+	ft_memcpy(res, s + start, len);
+	res[len] = '\0';
+	return (res);
+}
+
