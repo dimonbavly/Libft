ecdafda72f73b5b5192ce2704dc99766d40112e0
Stannis Rupert <srupert@un-g4.kzn.21-school.ru>
2021-05-11 10:42:59 +0300

    ft_substr is ready

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,10 +6,10 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 
 	len = ft_strlen(src);
 	if (len + 1 < size)
-		memcpy(dst, src, len + 1);
+		ft_memcpy(dst, src, len + 1);
 	else if (size != 0)
 	{
-		memcpy(dst, src, size - 1);
+		ft_memcpy(dst, src, size - 1);
 		dst[size - 1] = '\0';
 	}
 	return (len);
