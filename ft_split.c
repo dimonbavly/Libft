9ffd3efc7d0c6e508fc5ea1ac509cc7c71f2b8b6
Stannis Rupert <srupert@un-g4.kzn.21-school.ru>
2021-05-12 21:39:37 +0300

    ft_strmapi is ready

--- ft_split.c
+++ ft_split.c
@@ -1,96 +1,90 @@
 #include "libft.h"
 #include <stdlib.h>
 
-static size_t	ft_getstart(char const *s, char c);
-static size_t	ft_getend(char const *s, char c, size_t len_s);
-static size_t	ft_getamountchars(char const *s, char c, size_t start, size_t end);
-static void 	ft_freeall(char **dest, size_t elems);
-
-
-
+static size_t ft_countwords(char *str, char c);
+static size_t ft_split_handler(char *str, char c, char **dest);
 
 char	**ft_split(char const *s, char c)
 {
-	size_t	start;
-	size_t	end;
-	size_t	cntpos;
-	size_t	amountchars;
-	char	**res;
-	size_t holdamount;
-	end = ft_getend(s, c , ft_strlen(s));
-	start = ft_getstart(s, c);
-	amountchars = ft_getamountchars(s, c, start, end);
-	holdamount = amountchars;
-	res = malloc(sizeof(res) * (amountchars + 1));
-	if (!res)
-		return (NULL);
-	cntpos = start;
-	while (cntpos <= end)
-	{
-		if (s[cntpos] == c)
-		{
-			*res = ft_substr(s, start, cntpos - start);
-			if (!(*res))
-			{
-				while (*s == c)
-					s++;
-				ft_freeall(res, ft_getamountchars(s, c, 0, end) - amountchars);
-				return (NULL);
-			}
-			start = cntpos + 1;
-			res++;
-			amountchars--;
-		}
-		cntpos++;
-	}
-	return (res - holdamount);
-}
-
-static size_t	ft_getstart(char const *s, char c)
-{
-	size_t start;
-
-	start = 0;
-	while (s[start] && s[start] == c)
-		start++;
-	return (start);
-}
-
-static size_t	ft_getend(char const *s, char c, size_t len_s)
-{
-	while (s[len_s] >= 0 && s[len_s] == c)
-		len_s--;
-	return (len_s);
-}
-
-static size_t	ft_getamountchars(char const *s, char c, size_t start, size_t end)
-{
-	size_t	amountchars;
 	
-	amountchars = 0;
-	while (start <= end)
-	{
-		if (s[start] == c)
-			amountchars++;
-		start++;
-	}
-	return (amountchars);
+	char *str;
+	char **res;
+
+	str = ft_strtrim(s, &c);
+	if (!str)
+		return (NULL);
+	res = malloc(sizeof(*res) * ft_countwords(str, c));
+	if (ft_countwords(str,c) != ft_split_handler(str, c, res))
+		return (NULL);
+	return (res);
+
 }
 
-static void 	ft_freeall(char **dest, size_t elems)
+
+static size_t ft_countwords(char *str, char c)
 {
 	size_t cnt;
+	size_t occ;
 
+	
+	occ = 0;
 	cnt = 0;
-	while (cnt <= elems)
+	while (str[cnt])
 	{
-		free(dest[cnt]);
-		dest[cnt] = NULL;
-		cnt++;
+		if (str[cnt] == c)
+			cnt++;
+		else if (str[cnt + 1] == c || str[cnt + 1] == '\0')
+		{
+			cnt++;
+			occ++;
+		}	
+		else 
+			cnt++;
 	}
-	free(dest);
-	dest = NULL;
+	return (occ);
 }
 
+static size_t ft_split_handler(char *str, char c, char **dest)
+{
+	size_t cnt;
+	size_t occ;
+	size_t start;
+	
+	occ = 1;
+	cnt = 0;
+	while (str[cnt])
+	{
+		if (str[cnt] == c)
+			cnt++;
+		else if (!cnt)
+			start = cnt++;
+		else if (cnt)
+		{
+			if (str[cnt - 1] == c)
+				start = cnt;
+			cnt++;
+		}
+			else if (str[cnt + 1] == c)
+		{
+			str[cnt + 1] = '\0';
+			dest[occ - 1] = ft_strtrim(str + start, &c);
+			if (!dest[occ-1])
+				return (occ);
+			cnt++;
+			occ++;
 
+		}
+		else if (str[cnt + 1] == '\0')
+		{
+			dest[occ - 1] = ft_strtrim(str + start, &c);
+			if (!dest[occ-1])
+				return (occ);
+			cnt++;
+			occ++;
+		}
 
+		else 
+			cnt++;
+	}
+	return (occ);
+}
