9bb4773e8a60ff9214020a7d78c0c8606ba157b0
Stannis Rupert <srupert@un-g4.kzn.21-school.ru>
2021-05-13 04:03:07 +0300

    checkpoint for ft_itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -1,52 +1,41 @@
 #include "libft.h"
 #include <stdlib.h>
-//static int ft_abs(int nbr);
+
 char *ft_itoa(int n)
 {
 	int cnt;
-	int tmp_n;
+	int work_n;
 	int sign;
-	char buf[12];
 	char *str;
 	
-	//
+	sign = 1;
 	if (n < 0)
 		sign = -1;
 	else
 		n  = -n;
-	//
-	//
-	tmp_n = n;
+	work_n = n;
 	cnt = 0;
-	while (tmp_n / 10 < 0)
-	{
-		buf[cnt] =	-(tmp_n % 10) + '0';
-		tmp_n = tmp_n / 10;
+	while (work_n /= 10)
 		cnt++;
-	}
-	if (tmp_n / 10 == 0)
-	{
-		buf[cnt] = -(tmp_n % 10) + '0';
+	if (work_n / 10 == 0)
 		cnt++;
-	}
-		
 	if (sign == -1)
-		buf[cnt++] = '-';
-	buf[cnt] = '\0';
-
-	//
-	//
+		cnt++;
 	str = malloc(sizeof(*str) * (cnt + 1));
    	if (!str)
 		return (NULL);
-	//
-	//
-	tmp_n = 0;
-	while (buf[tmp_n])
-		str[tmp_n++] = buf[cnt-- - 1];
-	str[tmp_n] = '\0';
+	str[cnt--] = '\0';
+	work_n = n;
+	while (work_n / 10 < 0)
+	{
+		str[cnt--] = -(work_n % 10) + '0';
+	   	work_n = work_n / 10;;
+	}
+	if (work_n / 10 == 0)
+		str[cnt] = -(work_n % 10) + '0';
+	if (sign == -1)
+		str[--cnt] = '-';
 	return (str);
-
 }
 
 
