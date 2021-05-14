d421da3426288dd1c52a6b088cc2de0d068fdf94
Stannis Rupert <srupert@pr-a3.kzn.21-school.ru>
2021-05-14 19:14:26 +0300

    rewrite ft_strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -1,29 +1,26 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_strnstr.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: srupert <marvin@42.fr>                     +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/05/08 17:58:05 by srupert           #+#    #+#             */
-/*   Updated: 2021/05/10 21:40:05 by srupert          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "libft.h"
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	size_t	cnt;
 	size_t	needlesize;
+	size_t	haystacksize;
 
-	if ((*needle == '\0' && len == 0) || needle == haystack)
-		return ((char *)haystack);
+	haystacksize = ft_strlen(haystack);
 	needlesize = ft_strlen(needle);
-	cnt = 0;
-	while ((ft_strncmp(haystack + cnt, needle, needlesize) != 0) && cnt < len)
-		cnt++;
-	if ((haystack + cnt) == '\0' || cnt + needlesize >= len)
+	if (*needle == '\0')
+		return ((char *)haystack);
+	else if (len == 0)
 		return (NULL);
-	return ((char *)(haystack + cnt));
+	else if (*haystack == '\0')
+		return (NULL);
+	cnt = 0;
+	while ((cnt + needlesize <= haystacksize) && ft_strncmp(&haystack[cnt],needle,needlesize) && (cnt + needlesize <= len))
+	{
+		cnt++;
+	}
+	if ((cnt + needlesize > haystacksize) || (cnt + needlesize > len))
+		return (NULL);
+	return ((char *)haystack  + cnt);
+	
 }
