3d6d046f821d412f45bb74c4119ff391b54e90c6
Stannis Rupert <srupert@student.21-school.ru>
2021-07-27 23:07:23 +0300

    add new dllst function

---
+++ ft_dlclearttail.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "libft.h"
+
+void	ft_dlclearttail(t_dllist **dllst, void (*del) (void*))
+{
+	t_dllist	*current;
+
+	if (!dllst)
+		return ;
+	(*dllst) = (*dllst)->next;
+	current = *dllst;
+	while (current)
+	{
+		del(current->content);
+		*dllst = current->next;
+		free(current);
+		current = *lst;
+	}
+	return ;
+}
