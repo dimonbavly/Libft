671aa719627d56f72b5d5f2773acc3a244c22e4b
Stannis Rupert <srupert@student.21-school.ru>
2021-07-27 23:08:18 +0300

    add new dllst function

---
+++ ft_dlclearthead.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "libft.h"
+
+void	ft_dlclearthead(t_dllist **dllst, void (*del) (void*))
+{
+	t_dllist	*current;
+
+	if (!dllst)
+		return ;
+	(*dllst) = (*dllst)->prev;
+	current = *dllst;
+	while (current)
+	{
+		del(current->content);
+		*dllst = current->prev;
+		free(current);
+		current = *lst;
+	}
+	return ;
+}
