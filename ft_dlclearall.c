bda61c505c609f1dbe0a12c43187287998ce5dc2
Stannis Rupert <srupert@student.21-school.ru>
2021-08-03 00:29:07 +0300

    fix

--- ft_dlclearall.c
+++ ft_dlclearall.c
@@ -5,8 +5,8 @@ void	ft_dlclearall(t_dllist **dllst, void (*del) (void*))
 {
 	if (!dllst)
 		return ;
-	ft_dlclearthead(dllist, del);
-	ft_dlclearttail(dllist, del);
+	ft_dlclearthead(dllst, del);
+	ft_dlclearttail(dllst, del);
 	del(dllst->content);
 	free(dllst);
 	return ;
