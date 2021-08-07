15de2ba25ea4337ebbd37bdfadc32390b1609c39
Stannis Rupert <srupert@student.21-school.ru>
2021-08-07 15:50:31 +0300

    fix

--- ft_dlinsertafter.c
+++ ft_dlinsertafter.c
@@ -5,12 +5,17 @@ void	ft_dlinsertafter(t_dllist **dllst, t_dllist *new)
 	if (dllst == NULL)
 		return ;
 	if (*dllst == NULL)
+	{
+		new->next = NULL;
+		new->prev = NULL;
 		*dllst = new;
+	}
 	else
 	{
 		new->next = (*dllst)->next;
 		new->prev = *dllst;
-		*dllst = new;
+		(*dllst)->next->prev = new;
+		(*dllst)->next = new;
 	}
 	return ;
 }
--- ft_dlinsertbefore.c
+++ ft_dlinsertbefore.c
@@ -5,12 +5,17 @@ void	ft_dlinsertbefore(t_dllist **dllst, t_dllist *new)
 	if (dllst == NULL)
 		return ;
 	if (*dllst == NULL)
+	{
+		new->next = NULL;
+		new->prev = NULL;
 		*dllst = new;
+	}
 	else
 	{
 		new->prev = (*dllst)->prev;
 		new->next = *dllst;
-		*dllst = new;
+		(*dllst)->prev->next = new;
+		(*dllst)->prev = new;
 	}
 	return ;
 }
